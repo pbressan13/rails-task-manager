93e77627f14ac5bfca1867b24e722dc7d5df6b4e
Pedro Bressan <pbressan13@gmail.com>
2021-10-09 11:56:54 -0300

    Now users can create a new task from form, added option to create a completed task

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
+
+  # Create a new task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # View all tasks or a specific task
   get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks', to: 'tasks#index'
 end
